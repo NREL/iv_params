0b5f2a05cdb2d9ce5417c08f5d609ecd18c19861
Michael Deceglie <mdeceglie@users.noreply.github.com>
2022-10-24 11:13:44 -0600

    version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iv_params",
-    version="0.0.4",
+    version="0.0.5",
     author="Michael Deceglie",
     author_email="michael.deceglie@nrel.gov",
     description="Perform ASTM E1036 extraction of photovoltaic IV parameters",
